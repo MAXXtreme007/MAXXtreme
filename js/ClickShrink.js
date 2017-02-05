a584827c9f48284f0fe5f326b3530db6aa64941a
Matt Campbell <mattandhisapple@gmail.com>
2017-02-05 03:47:41 -0600

    Add Firebase & Gulp
    Begin work on database & authentication implementation.

--- js/ClickShrink.js
+++ js/ClickShrink.js
@@ -1,8 +1,8 @@
 function shrink() {
-        var img = document.getElementById('bolt')
-        if (img.width() > 100) {
-            img.animate({ width: "100px", height: "100px" }, 1000);
-        } else {
-            img.animate({ width: "200px", height: "200px" }, 1000);
-        }
+    var img = $("#bolt");
+    if (img.width() > 85 && img.height() > 100 ) {
+        img.animate({ width: "80px", height: "95px" }, 250);
+    } else {
+        img.animate({ width: "200px", height: "216px" }, 600);
+    }
 }
--- js/DynamicIcon.js
+++ js/DynamicIcon.js
@@ -4,6 +4,7 @@ logoSize = function() {
     var newImage = new Image();
     newImage.src = theLogo.attr("src");
     var imgWidth = newImage.width;
+    var imgHeight = newImage.height;
 
     // distance over which zoom effect takes place
     var maxScrollDistance = 500;
@@ -13,40 +14,46 @@ logoSize = function() {
 
     // width at maximum zoom out (i.e. when window has scrolled maxScrollDistance)
     var widthAtMax = 80;
+    var heightAtMax = 95;
 
     // calculate diff and how many pixels to zoom per pixel scrolled
     var widthDiff = imgWidth - widthAtMax;
-    var pixelsPerScroll = (widthDiff / maxScrollDistance);
+    var pixelsPerScrollH = (widthDiff / maxScrollDistance);
+    var heightDiff = imgHeight - heightAtMax;
+    var pixelsPerScrollV = (heightDiff / maxScrollDistance);
 
     $(window).scroll(function() {
         // the currently scrolled-to position - max-out at maxScrollDistance
         var scrollTopPos = Math.min($(document).scrollTop(), maxScrollDistance);
 
         // how many pixels to adjust by
-        var scrollChangePx = Math.floor(scrollTopPos * pixelsPerScroll);
+        var scrollChangePxH = Math.floor(scrollTopPos * pixelsPerScrollH);
+        var scrollChangePxV = Math.floor(scrollTopPos * pixelsPerScrollV);
 
         // calculate the new width
-        var zoomedWidth = imgWidth - scrollChangePx;
+        var zoomedWidth = imgWidth - scrollChangePxH;
+        var zoomedHeight = imgHeight - scrollChangePxV;
 
         // set the width
         $('#bolt').css('width', zoomedWidth);
+        $('#bolt').css('height', zoomedHeight);
     });
 }
 
-logoMove = function() {
-    // Create cross browser requestAnimationFrame method:
-    window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(f) { setTimeout(f, 1000 / 60) }
+// logoMove = function() {
+//     // Create cross browser requestAnimationFrame method:
+//     window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame || function(f) { setTimeout(f, 1000 / 60) }
 
-    var boltLogo = document.getElementById('bolt')
+//     var boltLogo = document.getElementById('bolt')
 
-    function parallax() {
-        var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
-        boltLogo.style.top = -scrolltop * .01 + 'px' // move bubble2 at 10% of scroll rate
-    }
+//     function parallax() {
+//         var scrolltop = window.pageYOffset // get number of pixels document has scrolled vertically
+//         boltLogo.style.top = -scrolltop * .01 + 'px' // move bubble2 at 10% of scroll rate
+//     }
 
-    window.addEventListener('scroll', function() { // on page scroll
-        requestAnimationFrame(parallax) // call parallaxbubbles() on next available screen paint
-    }, false)
-}
+//     window.addEventListener('scroll', function() { // on page scroll
+//         requestAnimationFrame(parallax) // call parallaxbubbles() on next available screen paint
+//     }, false)
+// }
 
 logoSize();
